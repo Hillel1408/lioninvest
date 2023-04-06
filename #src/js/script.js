c88d528af8b35715111ba12f5eb0283fe24cb447
Hillel1408 <etemax@bk.ru>
2023-04-06 11:11:31 +0300

    Lion invest

--- #src/js/script.js
+++ #src/js/script.js
@@ -9,16 +9,13 @@ navItems.forEach((item) => {
 
 const mobileMenuBtn = document.querySelector('.header__mobile-menu');
 const headerNav = document.querySelector('.header__nav');
-const modal = document.querySelector('.modal');
 mobileMenuBtn.addEventListener('click', () => {
     headerNav.classList.toggle('active');
     document.body.classList.toggle('lock');
-    modal.classList.toggle('active');
 });
 
 const mobileMenuCloseBtn = document.querySelector('.header__mobile-menu-icon');
 mobileMenuCloseBtn.addEventListener('click', () => {
     headerNav.classList.toggle('active');
     document.body.classList.toggle('lock');
-    modal.classList.toggle('active');
 });
